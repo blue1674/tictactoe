1b5828953ea15bdc6447b0b2daea147bb74c96fa
Manaf <f20190351@pilani.bits-pilani.ac.in>
2021-01-30 23:10:05 +0530

    Form radio functionality

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const game = (function () {
     let _markedBlocks = 0;
     let gameBoard = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
     let blocks = [];
-    let _setWinner; 
+    let _setWinner;
     function displayBoard() {
         for (let i = 0; i < 9; i++) {
             const block = document.createElement('div');
@@ -42,7 +42,7 @@ const game = (function () {
                     status = false;
                     break;
                 }
-                if(i == startIndex+2){
+                if (i == startIndex + 2) {
                 }
             }
             return status;
@@ -68,28 +68,29 @@ const game = (function () {
             return blocks[i].textContent === blocks[j].textContent;
         }
 
-        function getBlockText(i){
+        function getBlockText(i) {
             return blocks[i].textContent;
         }
-        function alertWinner(i){
-            alert(`winner = ${getBlockText(i)}`); 
+        function alertWinner(i) {
+            alert(`winner = ${getBlockText(i)}`);
+            document.location.reload();
         }
-        if(checkRows(0))
-        alertWinner(0); 
-        else if(checkRows(3))
-        alertWinner(3);
-        else if(checkRows(6))
-        alertWinner(6); 
-        else if(checkColumns(0))
-        alertWinner(0);
-        else if(checkColumns(1))
-        alertWinner(1);
-        else if(checkColumns(2))
-        alertWinner(2);
-        else if(checkDiagonal(0))
-        alertWinner(0); 
-        else if(checkDiagonal(2))
-        alertWinner(2); 
+        if (checkRows(0))
+            alertWinner(0);
+        else if (checkRows(3))
+            alertWinner(3);
+        else if (checkRows(6))
+            alertWinner(6);
+        else if (checkColumns(0))
+            alertWinner(0);
+        else if (checkColumns(1))
+            alertWinner(1);
+        else if (checkColumns(2))
+            alertWinner(2);
+        else if (checkDiagonal(0))
+            alertWinner(0);
+        else if (checkDiagonal(2))
+            alertWinner(2);
 
         return checkRows(0) || checkRows(3) || checkRows(6) || checkColumns(0) || checkColumns(1) || checkColumns(2) || checkDiagonal(0) || checkDiagonal(2);
     }
@@ -100,12 +101,50 @@ const game = (function () {
 const Player = function (name, option) {
     return { name, option };
 }
-let currentPlayer = 'X';
-const button = document.getElementById('playGame');
-button.addEventListener('click', function () {
-    document.location.reload();
+let currentPlayer;
+let player1;
+let player2;
+const form = document.querySelector('form');
+// const option2X = document.getElementById('option2X');
+// const option2O = document.getElementById('option2O');
+// const option1X = document.getElementById('option1X');
+// const option1O = document.getElementById('option1O');
+
+// function checkRadios(option1, option2, option3) {
+//     option1.addEventListener('click', function () {
+//         if(option1.checked && option2.checked)
+//         option3.setAttribute('checked', 'true'); 
+//     });
+// }
+
+// checkRadios(option1X, option2X, option2O); 
+// checkRadios(option1O, option2O, option2X); 
+// checkRadios(option2X, option1X, option1O); 
+// checkRadios(option2O, option1O, option1X); 
+const option2X = document.getElementById('option2X');
+const option2O = document.getElementById('option2O');
+const option1X = document.getElementById('option1X');
+const option1O = document.getElementById('option1O');
+
+check = () => document.querySelector('input[name="option1"]:checked') && document.querySelector('input[name="option2"]:checked');
+
+option1X.addEventListener("change", function() {if (check()) option2O.checked = true});
+option2X.addEventListener("change", function() {if (check()) option1O.checked = true});
+
+option1O.addEventListener("change", function() {if (check()) option2X.checked = true});
+option2O.addEventListener("change", function() {if (check()) option1X.checked = true});
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const formData = Object.fromEntries(new FormData(form));
+    currentPlayer = formData.option1;
+    form.setAttribute('hidden', 'true');
+    game.displayBoard();
 });
-game.displayBoard();
+
+
+
 
 
 
